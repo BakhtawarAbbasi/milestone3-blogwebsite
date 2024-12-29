cb74172e54ad12811fbdd4f3f23edb3a6b86e633
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2024-12-29 20:11:25 +0500

    updaate

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -7,6 +7,7 @@ export interface simpleBlogCard{
 export interface fullBlog {
     currentSlug: string,
     title: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     content: any;
     titleImage: string;
 
--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -7,6 +7,7 @@ export const client = createClient({
     useCdn: false,
 });
 const builder = imageUrlBuilder(client)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function urlFor(source: any){
     return builder.image(source);
 }
