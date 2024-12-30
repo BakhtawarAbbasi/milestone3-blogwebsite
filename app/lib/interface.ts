f960f728743eba48d49226ea4a46617a20f209f6
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2024-12-30 13:53:08 +0500

    update

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,14 +1,13 @@
 import type { TypedObject } from "@sanity/types";
 
-export interface simpleBlogCard {
+export interface simpleBlogCard{
     title: string;
     smallDescription: string;
     currentSlug: string;
-    titleImage: string;
+    titleImage: string; 
 }
-
 export interface fullBlog {
-    currentSlug: string;
+    currentSlug: string,
     title: string;
     content: TypedObject | TypedObject[];
     titleImage: string;
--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,16 +1,13 @@
-import { createClient } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
+import { createClient} from "next-sanity";
+import imageUrlBuilder from '@sanity/image-url';
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
-
 export const client = createClient({
-    apiVersion: "2023-05-03",
-    dataset: "production",
-    projectId: "aedtb60l",
-    useCdn: false, // Set to true if you want cached data
+    apiVersion: '2023-05-03',
+    dataset: 'production',
+    projectId: 'aedtb60l',
+    useCdn: false,
 });
-
-const builder = imageUrlBuilder(client);
-
-export function urlFor(source: SanityImageSource) {
+const builder = imageUrlBuilder(client)
+export function urlFor(source: SanityImageSource){
     return builder.image(source);
 }
