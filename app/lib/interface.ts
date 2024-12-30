57c0ab53c39b6ebb6a303211c6ec9092a2db17e7
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2024-12-30 21:02:04 +0500

    milestone3

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -10,6 +10,7 @@ export interface simpleBlogCard{
 export interface fullBlog {
     currentSlug: string,
     title: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     content: any;
     titleImage: string;
 }
